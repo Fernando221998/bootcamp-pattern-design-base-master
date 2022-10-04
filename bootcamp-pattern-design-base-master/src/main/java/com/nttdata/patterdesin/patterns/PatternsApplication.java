9e81035ddbf7c73a8c7e9221b7b2bcf5c49fa141
Fernando221998 <94843540+Fernando221998@users.noreply.github.com>
2022-10-04 12:15:38 +0200

    Update PatternsApplication.java

--- bootcamp-pattern-design-base-master/src/main/java/com/nttdata/patterdesin/patterns/PatternsApplication.java
+++ bootcamp-pattern-design-base-master/src/main/java/com/nttdata/patterdesin/patterns/PatternsApplication.java
@@ -79,12 +79,12 @@ public class PatternsApplication implements CommandLineRunner {
 		System.out.println(per.getName() + " Edad: " + per.getAge() + "- " + per);
 		
 		System.out.println("=============================================================================");
-        System.out.println("Personas entre 20 y 30 años");
-        System.out.println("=============================================================================");
-        
-        PersonFacade facade = new PersonFacade();
-        List<Person> foundPersons = facade.getPersonsEntreEdad(20, 30);
-        foundPersons.forEach(x -> System.out.println(x.getName()+", edad: "+x.getAge()));
+		System.out.println("Personas entre 20 y 30 años");
+		System.out.println("=============================================================================");
+
+		PersonFacade facade = new PersonFacade();
+		List<Person> foundPersons = facade.getPersonsEntreEdad(20, 30);
+		foundPersons.forEach(x -> System.out.println(x.getName()+", edad: "+x.getAge()));
 		
 		System.out.println("=============================================================================");
 		System.out.println("Prácticas con Decorador");
