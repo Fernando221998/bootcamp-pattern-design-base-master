ad96c5f08d3870c9c61a80eebcc03318d5386157
Fernando221998 <94843540+Fernando221998@users.noreply.github.com>
2022-10-04 12:17:21 +0200

    Update PatternsApplication.java

--- bootcamp-pattern-design-base-master/src/main/java/com/nttdata/patterdesin/patterns/PatternsApplication.java
+++ bootcamp-pattern-design-base-master/src/main/java/com/nttdata/patterdesin/patterns/PatternsApplication.java
@@ -59,55 +59,55 @@ public class PatternsApplication implements CommandLineRunner {
 		bussines.ejecuta();
 		
 		
-		System.out.println("=============================================================================");
-		System.out.println("Prácticas con Prototype");
-		System.out.println("=============================================================================");
+	System.out.println("=============================================================================");
+	System.out.println("Prácticas con Prototype");
+	System.out.println("=============================================================================");
 
-		Person fernando = new Person("Fernando", 42);
-		Person fernandoClon = fernando.clone();
+	Person fernando = new Person("Fernando", 42);
+	Person fernandoClon = fernando.clone();
 
-		System.out.println("ID del objeto ismael: " + fernando.getName() + " Edad: " + fernando.getAge() + "- " + fernando);
-		System.out.println("ID del objeto ismaelClon: " + fernandoClon.getName() + " Edad: " + fernandoClon.getAge() + " - "
-				+ fernandoClon);
-		
-		System.out.println("=============================================================================");
-		System.out.println("Prácticas con Builder");
-		System.out.println("=============================================================================");
+	System.out.println("ID del objeto ismael: " + fernando.getName() + " Edad: " + fernando.getAge() + "- " + fernando);
+	System.out.println("ID del objeto ismaelClon: " + fernandoClon.getName() + " Edad: " + fernandoClon.getAge() + " - "
+			+ fernandoClon);
 
-		Person per = Person.Builder().name("Fernando").age(152).build();
+	System.out.println("=============================================================================");
+	System.out.println("Prácticas con Builder");
+	System.out.println("=============================================================================");
 
-		System.out.println(per.getName() + " Edad: " + per.getAge() + "- " + per);
-		
-		System.out.println("=============================================================================");
-		System.out.println("Personas entre 20 y 30 años");
-		System.out.println("=============================================================================");
+	Person per = Person.Builder().name("Fernando").age(152).build();
 
-		PersonFacade facade = new PersonFacade();
-		List<Person> foundPersons = facade.getPersonsEntreEdad(20, 30);
-		foundPersons.forEach(x -> System.out.println(x.getName()+", edad: "+x.getAge()));
-		
-		System.out.println("=============================================================================");
-		System.out.println("Prácticas con Decorador");
-		System.out.println("=============================================================================");
-		
-		PersonInterface person2 = Person.Builder().age(42).name("Fernando Cornejo Hernández").build();
-		PersonInterface person3 =  new PersonDecorator(person2);
-		
-		System.out.println("Prototype Name " + person2.getName());
-		System.out.println("Prototype Decorated Name " + person3.getName());
-		
-		System.out.println("Prototype Edad Meses " + person2.getAge());
-		System.out.println("Prototype Decorated Edad Días " + person3.getAge());
-		
-		System.out.println("=============================================================================");
-		System.out.println("Prácticas con Proxy");
-		System.out.println("=============================================================================");
+	System.out.println(per.getName() + " Edad: " + per.getAge() + "- " + per);
 
-		Person entidad = Person.Builder().age(24).name("Fernando Cornejo").build();
-		
-		AbstractPersonProxy proxy = new PersonProxy(entidad);
-		
-		proxy.realiza();
+	System.out.println("=============================================================================");
+	System.out.println("Personas entre 20 y 30 años");
+	System.out.println("=============================================================================");
+
+	PersonFacade facade = new PersonFacade();
+	List<Person> foundPersons = facade.getPersonsEntreEdad(20, 30);
+	foundPersons.forEach(x -> System.out.println(x.getName()+", edad: "+x.getAge()));
+
+	System.out.println("=============================================================================");
+	System.out.println("Prácticas con Decorador");
+	System.out.println("=============================================================================");
+
+	PersonInterface person2 = Person.Builder().age(42).name("Fernando Cornejo Hernández").build();
+	PersonInterface person3 =  new PersonDecorator(person2);
+
+	System.out.println("Prototype Name " + person2.getName());
+	System.out.println("Prototype Decorated Name " + person3.getName());
+
+	System.out.println("Prototype Edad Meses " + person2.getAge());
+	System.out.println("Prototype Decorated Edad Días " + person3.getAge());
+
+	System.out.println("=============================================================================");
+	System.out.println("Prácticas con Proxy");
+	System.out.println("=============================================================================");
+
+	Person entidad = Person.Builder().age(24).name("Fernando Cornejo").build();
+
+	AbstractPersonProxy proxy = new PersonProxy(entidad);
+
+	proxy.realiza();
 
         System.out.println("Fin...");
     }
